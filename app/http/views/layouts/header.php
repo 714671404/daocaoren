7884501cd43885dcba981b5aa11ccb7892222c1a
714671404 <714671404@qq.com>
2020-04-17 18:42:22 +0800

    update

---
+++ app/http/views/layouts/header.php
@@ -0,0 +1,22 @@
+<header class="header">
+	<nav class="navbar">
+		<a href="/" class="logo"><img src="/images/logo.png"></a>
+		<ul class="nav">
+			<li><a href="">首页</a></li>
+			<li><a href="">HTML</a></li>
+			<li><a href="">CSS</a></li>
+			<li><a href="">JavaScript</a></li>
+			<li><a href="">PHP</a></li>
+			<li><a href="">Python</a></li>
+		</ul>
+		<div class="auth">
+			<a href="login" class="btn login-btn">登录</a>
+			<a href="register" class="btn register-btn">注册</a>
+		</div>
+		<div class="select">
+			<form>
+				<input type="text" class="select-input" placeholder="输入搜索内容">
+			</form>
+		</div>
+	</nav>
+</header>
