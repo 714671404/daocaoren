290a44e60ff80bc2a07780c632cde7a94b0b621a
714671404 <714671404@qq.com>
2020-04-18 18:06:55 +0800

    update

--- public/log.sql
+++ public/log.sql
@@ -1,22 +1,23 @@
 -- 创建数据库
-CREATE DATABASE notebook CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE daocaoren CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- 创建表用户表
-CREATE TABLE users(
-	id int primary key auto_increment,
-	name varchar(80),
-	username varchar(80),
-	password varchar(80),
-	email varchar(80),
-	phone varchar(80),
-	created_at datetime DEFAULT NOW() COMMENT '注册时间',
-	updated_at datetime DEFAULT NOW() COMMENT '更新时间'
-)CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- CREATE TABLE users(
+-- 	id int primary key auto_increment,
+-- 	name varchar(80),
+-- 	username varchar(80),
+-- 	password varchar(80),
+-- 	email varchar(80),
+-- 	phone varchar(80),
+-- 	created_at datetime DEFAULT NOW() COMMENT '注册时间',
+-- 	updated_at datetime DEFAULT NOW() COMMENT '更新时间'
+-- )CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- 创建文章表
 CREATE TABLE articles(
 	`id` int primary key auto_increment,
-	`user_id` int,
+-- 	`user_id` int,
+	`name`	varchar(255),
 	`title` varchar(255),
 	`content` text,
 	`created_at` datetime DEFAULT NOW() COMMENT '文章创建时间',
