e7355ff736d493ba97a98a8f7eaa52fa6d32164e
714671404 <714671404@qq.com>
2020-04-19 17:31:01 +0800

    update

--- resources/lang/en/validator.php
+++ resources/lang/en/validator.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-	'required' => ''
+	'required' => '{attribute}不可以为空.',
+	'string' => '{attribute}必须为字符串.',
+	'min' => '{attribute}不能小于{min}.',
+	'max' => '{attribute}不能大于{max}.'
 ];
