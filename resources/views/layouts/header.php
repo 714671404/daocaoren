1f61181c7fc4ea40fe6f632e7cf77d7a4f94a684
714671404 <714671404@qq.com>
2020-04-25 02:35:06 +0800

    update

--- resources/views/layouts/header.php
+++ resources/views/layouts/header.php
@@ -9,14 +9,28 @@
 			<li><a href="">PHP</a></li>
 			<li><a href="">Python</a></li>
 		</ul>
+        <div class="select">
+            <form>
+                <input type="text" class="select-input" placeholder="输入搜索内容">
+            </form>
+        </div>
 		<div class="auth">
-			<a href="/login" class="btn login-btn">登录</a>
-			<a href="/register" class="btn register-btn">注册</a>
-		</div>
-		<div class="select">
-			<form>
-				<input type="text" class="select-input" placeholder="输入搜索内容">
-			</form>
+            <?php if (!isset($_SESSION['username'])) {?>
+            <a href="/login" class="btn login-btn">登录</a>
+            <a href="/register" class="btn register-btn">注册</a>
+            <?php } else { ?>
+            <div class="user">
+                <img src="/images/avatar/default.jpg">
+                <ul class="nav">
+                    <li><a href="/">个人中心</a></li>
+                    <li><a href="/">个人中心</a></li>
+                    <li><a href="/">个人中心</a></li>
+                    <li><a href="/">个人中心</a></li>
+                    <li><a href="/logout">退出</a></li>
+                </ul>
+            </div>
+
+            <?php }?>
 		</div>
 	</nav>
 </header>
