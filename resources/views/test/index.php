f2c88650322fa8190205b25e84b09fe4b8e365d6
714671404 <714671404@qq.com>
2020-05-20 08:46:35 +0800

    update

--- resources/views/test/index.php
+++ resources/views/test/index.php
@@ -6,32 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link href="/css/bootstrap-3.3.7/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap-3.3.7/bootstrap.min.css" rel="stylesheet" type="text/css">
 </head>
     <div id="app">
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-            Launch demo modal
-        </button>
-
-        <!-- Modal -->
-        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-                    </div>
-                    <div class="modal-body">
-                        ...
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div class="container"></div>
     </div>
 
     <script src="/js/jquery/jquery-3.5.1.min.js"></script>
